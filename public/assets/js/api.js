cb8cd3d7e4e7f7f532dc448e99d9d86863e349a8
Jonathan Tiritilli <31551707+jontiritilli@users.noreply.github.com>
2018-06-21 00:31:41 -0700

    confirmed access to api

--- public/assets/js/api.js
+++ public/assets/js/api.js
@@ -1,35 +1,40 @@
 //jQuery functionality to run a function when the website is loaded
-document.ready(initializeApp);
-const api = require('./apiKey.js');
+$(document).ready(initializeApp);
+import { api } from './apiKey.js';
 
 //function to run on document ready
-const initializeApp = () => {
+function initializeApp() {
   //adding click functionality to the page
   addHandlers();
 }
 
-const addHandlers = () => {
+function addHandlers() {
   //jquery library being accessed to get page elements
-  $('.formSubmit').on('click', () => {
-    httpsRequest();
+  $('#formSubmit').on('click', () => {
+    httpsRequest(collectUserData());
   })
 }
 
-const httpsRequest = () => {
-
+function collectUserData(){
+  let id = $('#userId').val()
+  console.log(id);
+  return id;
+}
+function httpsRequest(gamer) {
   $.ajax({
-    url: 'https://xboxapi.com/v2/accountxuid',
-    data: {'X-AUTH': api},
+    url: 'https://xboxapi.com/v2/'+gamer+'/xbox360games',
+    method: 'GET',
+    beforeSend: request => {
+      request.setRequestHeader("X-Auth", api.key);
+    },
+    // data: {'gamertag': gamertag},
     dataType: 'JSON',
-    success: res => {
-      let response = JSON.parse(res);
-      console.log(response);
+    success: res => {;
+      console.log(res);
     },
     error: (xhr, errText, err) => {
-      console.log('jqXHR ' + jqXHR);
       console.log('error text '+ errText);
-      console.log('error code' + err);
+      console.log('error code ' + err);
     },
-
   })
 }
