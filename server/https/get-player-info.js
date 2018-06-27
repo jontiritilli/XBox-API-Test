6ec233dd1a2c7b26a6a57a425404f0ee9c0a9f6c
Jonathan Tiritilli <31551707+jontiritilli@users.noreply.github.com>
2018-06-26 17:29:55 -0700

    drafting sql statements

--- server/https/get-player-info.js
+++ server/https/get-player-info.js
@@ -1,6 +1,16 @@
 const axios = require('axios');
+const mysql = require('mysql');
 
-export async function get_player_info(gamertag){
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'me',
+  password : 'secret',
+  database : 'my_db'
+});
+
+connection.connect();
+
+export async function get_user_info_https(gamertag){
   this.gamertag = gamertag;
   try {
     this.xuid = await axios({
@@ -8,10 +18,30 @@ export async function get_player_info(gamertag){
       url: 'https://xboxapi.com/v2/xuid/' + this.gamertag,
       responseType:'json'
     })
-    .then(function(response) {
-      //write sql here
+    .then(function(response, xuid) {
+      let sql = 'INSERT INTO users (xuid, tag, stats) VALUES (?, ?, ?)';
+      connection.query(sql, xuid, this.gamertag, response, function(err, result){
+        if(err){
+          throw err;
+
+        }
+        console.log('insert confirmed successessful')
+      });
     })
   } catch (e) {
     console.log('There was an error: '+e)
   }
 };
+
+// export async function update_player_info(){
+//   this.gamertag = gamertag;
+//   try {
+//     this.xuid
+//     })
+//     .then(function(response) {
+//       //write sql here
+//     })
+//   } catch (e) {
+//     console.log('There was an error: '+e)
+//   }
+// };
